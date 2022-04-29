e8b45f6c2d3b96a013fb618af09e58044f3b5901
Thibault-Guignard <thibault.guignard.86@gmail.com>
2022-04-29 13:55:43 +0200

    mise en place route home sur MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
     public function home()
     {
-        $response = new Response('je suis la home');
+        //echo "Je suis la home !";
 
+        $response = new Response("<h1>Je suis la home !</h1>");
         return $response;
     }
 }
