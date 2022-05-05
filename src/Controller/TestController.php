62680d6ac03ae5814a748bb6d2fc96217a2d56cc
Thibault-Guignard <thibault.guignard.86@gmail.com>
2022-05-05 13:38:35 +0200

    ENTITY + MIGRATION

---
+++ src/Controller/TestController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Genre;
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Expérimentation Doctrine
+ */
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test/genre/add", name="test_genre_add")
+     */
+    public function addGenre()
+    {
+        // On crée un objet Genre
+        $fantastique = new Genre();
+        // On le définit
+        $fantastique->setName('Fantastique');
+
+        dd($fantastique);
+    }
+
+    /**
+     * Browse : lister tous les films!
+     * 
+     * @Route("/test/movie/browse",name="movie_browse")
+     */
+    public function movieBrowse(MovieRepository $movieRepository)
+    {
+        //on recupere tous les films depuis le Repository de Move
+        $moviesList = $movieRepository->findAll();
+
+        dd($moviesList);
+    }
+
+    /**
+     * Read : recuêrer un film!
+     * 
+     * @Route("/test/movie/read/{id}",name="movie_read")
+     */
+    public function movieRead($id, MovieRepository $movieRepository)
+    {
+        $movie = $movieRepository->find($id);
+
+        // todo@ gérer la 404
+
+        dd($movie);
+    }
+
+    /**
+     * Edit : editer un films!
+     * 
+     * @Route("/test/movie/edit/{id}",name="movie_edit")
+     */
+    public function movieEdit($id, ManagerRegistry $doctrine)
+    {
+        //1. on recupere l'objet a modifier
+        $movie = $doctrine->getRepository(Movie::class)->find($id);
+
+        // todo@ gérer la 404
+
+        //2. on modifier la duré au hasars
+        $movie->setDuration(mt_rand(30,180));
+
+        //3. on sauvegarde
+        $entityManager = $doctrine->getManager();
+        // /!\ pas de persist car entité deja persisté elle a un id 
+        //on la flushe
+        $entityManager->flush();
+
+        dd($movie);
+    }
+
+    /**
+     * Add : ajouter un film!
+     * 
+     * @Route("/test/movie/add",name="movie_add")
+     */
+    public function movieAdd(ManagerRegistry $doctrine)
+    {
+        //on crée un objet de type movie
+        $movie = new Movie();
+        //on defiinit ses propriétés
+        $movie->setTitle('Doctor Strange');
+        $movie->setReleaseDate(new DateTime('2014-11-05'));
+        $movie->setDuration('125');
+
+        dump($movie);
+
+        $entityManager = $doctrine->getManager();
+        //on lui demande de persister notre entité de preparer notre requete inser
+        $entityManager->persist($movie);
+        //on demande d'execture la ou les requetes SQL associées
+        $entityManager->flush();
+
+        dump($movie);
+
+                // Pour accrocher ma toolbar :D
+                return new Response('</body>');
+    }
+
+    /**
+     * Delete : supprimer un  film!
+     * 
+     * @Route("/test/movie/delete/{id}",name="movie_delete")
+     */
+    public function movieDelete($id, ManagerRegistry $doctrine)
+    {
+        $movie = $doctrine->getRepository(Movie::class)->find($id);
+
+        // todo@ gérer la 404
+
+        //2. on supprime
+        $entityManager = $doctrine->getManager();
+        // /!\ pas de persist car entité deja persisté elle a un id 
+        $entityManager->remove($movie);
+        //on la flushe
+        $entityManager->flush();
+
+        dd('Entité supprimé',$movie);
+    }
+}
