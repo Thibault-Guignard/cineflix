9b427cb28a8b0d3bc46f0eee11e66978cbb056d9
Thibault-Guignard <thibault.guignard.86@gmail.com>
2022-05-17 11:42:12 +0200

    data-transformer pour Role dans form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -29,13 +30,30 @@ class UserType extends AbstractType
                     'Manager' => 'ROLE_MANAGER',
                     'Utilisateur' => 'ROLE_USER',
                 ],
-                'multiple' => true,
+                'multiple' => false,
                 'expanded' => true,
                 'help' => 'Un seul choix possible'
 
             ])
 
         ;
+        
+        //ajout d'un data transfomer
+        //pour convertir la chaine choisie en un tableau
+        //qui contient cette chaine et vice versa
+        $builder->get('roles')
+        ->addModelTransformer(new CallbackTransformer(
+            //de l'entité vers le form (affiche form)
+            function ($rolesAsArray) {
+                // transform the array to a string
+                return implode(', ', $rolesAsArray);
+            },
+            //du form vers l'Entité (traite form)
+            function ($rolesAsString) {
+            // transform the string back to an array
+                return explode(', ', $rolesAsString);
+            }
+        ));
 
         // On pourrait utiliser un DataTransformer pour ne gérer
         // qu'un seul rôle dans le form, et le convertir en tableau
