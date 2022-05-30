e4861f86cd653b3886a0710b2ef02e3df3cda017
Thibault-Guignard <thibault.guignard.86@gmail.com>
2022-05-30 15:07:36 +0200

    Test Acces Back End

---
+++ tests/Back/MovieTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Tests\Back;
+
+use PhpParser\Node\Stmt\Foreach_;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MovieTest extends WebTestCase
+{
+    public function testUserPageAccess(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('user@user.com');
+
+        $client->loginUser($testUser);
+
+        $securedRoutes = [
+            '/back/movie',
+            '/back/user',
+            '/back/user/new'
+        ];
+
+        foreach ($securedRoutes as $currentRoutes) {
+            $client->request('GET',$currentRoutes);
+            $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+        }
+
+    }
+
+    public function testManagerPageAccess()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('manager@manager.com');
+
+        $client->loginUser($testUser);
+
+        $forbiddenRoutes = [
+            '/back/user/new',
+            '/back/user',
+        ];
+        $authorizededRoutes = [
+            '/back/movie',
+        ];
+
+        foreach ($forbiddenRoutes as $currentRoutes) {
+            $client->request('GET',$currentRoutes);
+            $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+        };
+
+        foreach ($authorizededRoutes as $currentRoutes) {
+            $client->request('GET',$currentRoutes);
+            $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        };
+
+    }
+
+        /**
+     * @dataProvider routesForUser
+     * 
+     * @return void
+     */
+    public function testStandardUserPageAccess($urlToTest, $expectedCode, $httpMethod): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $standardUser = $userRepository->findOneByEmail('user@user.com');
+
+        // simulate $standardUser being logged in
+        $client->loginUser($standardUser);
+
+        // test e.g. the profile page
+        $client->request($httpMethod, $urlToTest);
+        $this->assertResponseStatusCodeSame($expectedCode);
+    }
+
+    public function routesForUser()
+    {
+        return [
+            ['/back/movie/new', Response::HTTP_FORBIDDEN, Request::METHOD_POST],
+            ['/back/user/new' , Response::HTTP_FORBIDDEN, Request::METHOD_POST],
+
+            ['/back/movie', Response::HTTP_FORBIDDEN, Request::METHOD_GET],
+            ['/back/user', Response::HTTP_FORBIDDEN, Request::METHOD_GET],
+            ['/back/user/new', Response::HTTP_FORBIDDEN, Request::METHOD_GET],
+        ];
+    }
+}
+
--- tests/Front/HomepageTest.php
+++ tests/Front/HomepageTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Front;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomepageTest extends WebTestCase
 {
@@ -21,5 +22,28 @@ class HomepageTest extends WebTestCase
         $filteredCrawler = $crawler->filter('div.movie__poster');
         $this->assertEquals(10, count($filteredCrawler));
 
+    }
+
+    public function testAdd2FavoriteFromHomepage()
+    {
+        $client = static::createClient();
+        $crawler= $client->request('GET','/');
+
+        //dump(count($crawler->filter('.bi-bookmark-plus')->eq(0)));
+        $buttonCrawlerNode = $crawler->filter('.movie__favorite > button')->eq(0);
+
+        $form = $buttonCrawlerNode->form();
+
+        //on soumet le form
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        //on suis la redirection ,
+        $crawler = $client->followRedirect();
+        //est ce qu'on est sur la route favoirtes
+
+        //est ce que j'ai le flash message
+        $this->assertSelectorTextContains('.alert-success', 'a été ajouté de votre liste de favoris');
+        //est ce qu'il y a bien un film
     }
 }
